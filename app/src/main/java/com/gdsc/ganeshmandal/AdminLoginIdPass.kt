15df8d3f058d3ecb17ac09b457d0fe1cb2bfc0df
Sanket Mane <111991599+Sanketmane2323@users.noreply.github.com>
2024-08-23 00:18:02 +0530

    changes made in front page

--- app/src/main/java/com/gdsc/ganeshmandal/AdminLoginIdPass.kt
+++ app/src/main/java/com/gdsc/ganeshmandal/AdminLoginIdPass.kt
@@ -13,8 +13,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -23,10 +27,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,6 +58,15 @@ class AdminLoginIdPass : ComponentActivity() {
                     var adminPass by remember {
                         mutableStateOf(TextFieldValue(""))
                     }
+
+                    var password by rememberSaveable { mutableStateOf("") }
+                    var passwordVisibility by remember { mutableStateOf(false) }
+
+                    val icon = if (passwordVisibility)
+                        painterResource(id = R.drawable.baseline_visibility_24)
+                    else
+                        painterResource(id = R.drawable.baseline_visibility_off_24)
+
                     Column(
                         verticalArrangement = Arrangement.Center,
                         modifier = Modifier
@@ -68,6 +86,15 @@ class AdminLoginIdPass : ComponentActivity() {
                             label = {
                                 Text(text = "Enter Id")
                             },
+                            trailingIcon = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.baseline_account_circle_24),
+                                    contentDescription = "null",
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .size(24.dp)
+                                )
+                            },
                             modifier = Modifier
                                 .fillMaxWidth()
                         )
@@ -84,27 +111,59 @@ class AdminLoginIdPass : ComponentActivity() {
                             label = {
                                 Text(text = "Enter Password")
                             },
+                            trailingIcon = {
+                                IconButton(
+                                    onClick = {
+                                        passwordVisibility = !passwordVisibility
+                                    },
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                        .size(24.dp)
+                                ) {
+                                    Icon(
+                                        painter = icon,
+                                        contentDescription = "Visibility Icon",
+                                    )
+                                }
+                            },
                             modifier = Modifier
-                                .fillMaxWidth()
+                                .fillMaxWidth(),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Password
+                            ),
+                            visualTransformation = if (passwordVisibility) VisualTransformation.None
+                            else PasswordVisualTransformation()
+
                         )
                         Spacer(modifier = Modifier.height(16.dp))
-                        Button(onClick ={
-                            if(
+                        Button(onClick = {
+                            if (
                                 !TextUtils.isEmpty(adminId.text) &&
                                 !TextUtils.isEmpty(adminPass.text)
-                            ){
-                                if(Global.listOfAdminIdAndPassword[adminId.text] == adminPass.text){
+                            ) {
+                                if (Global.listOfAdminIdAndPassword[adminId.text] == adminPass.text) {
                                     Global.adminId = adminId.text
-                                    Toast.makeText(this@AdminLoginIdPass, "Logged In Successfully", Toast.LENGTH_SHORT).show()
-                                    val intent = Intent(this@AdminLoginIdPass, AdminOptions::class.java)
+                                    Toast.makeText(
+                                        this@AdminLoginIdPass,
+                                        "Logged In Successfully",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    val intent =
+                                        Intent(this@AdminLoginIdPass, AdminOptions::class.java)
                                     startActivity(intent)
+                                } else {
+                                    Toast.makeText(
+                                        this@AdminLoginIdPass,
+                                        "InCorrect Password",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
                                 }
-                                else{
-                                    Toast.makeText(this@AdminLoginIdPass, "InCorrect Password", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                            else{
-                                Toast.makeText(this@AdminLoginIdPass, "Fill all Fields", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(
+                                    this@AdminLoginIdPass,
+                                    "Fill all Fields",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
                         }) {
                             Text(
